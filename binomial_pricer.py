6a3e1a5cd8849f1fef90d507cd17d1f4d34a879d
Matvey Ivanov <swim.enthusiast@gmail.com>
2025-07-14 15:28:29 -0400

    Q probs done

--- binomial_pricer.py
+++ binomial_pricer.py
@@ -1,5 +1,14 @@
-def hello():
-    return "👋 Hello, GitHub!"
-
-if __name__ == "__main__":
-    print(hello())
+import numpy as np
+import matplotlib as plt
+def choose(k, n):
+    a=1
+    for i in range(k):
+        a*=(n-i)/(i+1)
+    return a
+#Calculate Martingale Probability
+def martingale_up(u, d, interest_rate, time):
+    interest=exp(interest_rate*time)
+    assert u > interest and d< interest, "By NA condition"
+    return (u-interest)/(u-d)
+def martingale_down():
+    return 1-martingale_up()
